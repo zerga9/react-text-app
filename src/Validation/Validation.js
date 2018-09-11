8cab6d412670c12e02b08484ac1f475878e80f07
Gadiza Zerari <gadizaz@hotmail.com>
2018-09-11 11:10:19 +0100

    Output either text too short or long enough depending on length

--- src/Validation/Validation.js
+++ src/Validation/Validation.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
 const validation = (props) => {
+
+  let validationMessage = 'Text long enough';
+
+  if (props.inputLength <= 5) {
+    validationMessage = "Text too short";
+  }
   return (
     <div>
-      <p>Text too short</p>
+      <p>{validationMessage}</p>
     </div>
   );
 
