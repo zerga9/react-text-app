4195d8da636246a9d9772d1d3bdb8ce3eccb19de
Gadiza Zerari <gadizaz@hotmail.com>
2018-09-11 11:54:13 +0100

    Add App.css to align app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Validation from './Validation/Validation';
 import Char from './Char/Char';
+import './App.css'
 class App extends Component {
   state = {
     userInput: ''
