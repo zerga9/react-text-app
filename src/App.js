3639458faf7167e6ba44c8935551f41a075fe8bb
Gadiza Zerari <gadizaz@hotmail.com>
2018-09-11 11:33:56 +0100

    When clicking on charComponent it is removed

--- src/App.js
+++ src/App.js
@@ -8,10 +8,24 @@ class App extends Component {
   inputChangedHandler = (event) => {
     this.setState({userInput: event.target.value})
   }
+
+  deleteCharHandler = ( index ) => {
+    const text = this.state.userInput.split('')
+    text.splice(index, 1)
+    const updatedText = text.join('');
+    this.setState({userInput: updatedText})
+  }
+
   render() {
+
     const charList = this.state.userInput.split('').map((ch, index) => {
-      return <Char character={ch} key={index} />
+      return <Char
+        character={ch}
+        key={index}
+        clicked={() => this.deleteCharHandler(index)}
+      />
     });
+
     return (
       <div className="App">
         <ol>
--- src/Char/Char.js
+++ src/Char/Char.js
@@ -10,7 +10,7 @@ const char = (props) => {
   };
 
   return (
-    <div style={style}>
+    <div style={style} onClick={props.clicked}>
       {props.character}
     </div>
   );
